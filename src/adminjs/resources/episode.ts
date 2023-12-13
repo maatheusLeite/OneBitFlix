3d6b2f04eac782bc637bff9b0ccff10102f86c62
Matheus Leite <maatheus2710@gmail.com>
2023-12-13 12:42:53 -0300

    Adding file upload in adminjs

--- src/adminjs/resources/episode.ts
+++ src/adminjs/resources/episode.ts
@@ -1,4 +1,6 @@
-import { ResourceOptions } from "adminjs";
+import uploadFileFeature from "@adminjs/upload";
+import { FeatureType, ResourceOptions } from "adminjs";
+import path from "path";
 
 export const episodeResourceOptions: ResourceOptions = {
     navigation: 'Catálogo',
@@ -7,3 +9,20 @@ export const episodeResourceOptions: ResourceOptions = {
     listProperties: ['id', 'name', 'courseId', 'order', 'secondsLong'], // Propriedades listaveis
     showProperties: ['id', 'name', 'synopsis', 'courseId', 'order', 'videoUrl', 'secondsLong', 'createdAt', 'updatedAt'] // Propriedades que podem ser mostradas
 }
+
+export const episodeResourceFeatures: FeatureType[] = [
+    uploadFileFeature({
+        // UPLOAD OPTIONS
+        provider: { // Provider pode ser local, AWS, googleCloud, etc...
+            local: {
+                bucket: path.join(__dirname, '..', '..', '..', 'uploads')
+            }
+        },
+        properties: {
+            key: 'videoUrl', // coluna de referencia da url do video salvo, no banco de dados
+            file: 'uploadVideo' // input onde vai ser feito o envio do formulário do video no adminjs
+                                // basicamente, está lincando o campo uploadVideo do editProperties acima com a coluna videoUrl do banco de dados
+        },
+        uploadPath: (record, fileName) => `videos/course-${record.get('courseId')}/${fileName}`
+    })
+]
