d59eae26f8e421e3824b25c7c0cb0425a90da125
Matheus Leite <maatheus2710@gmail.com>
2023-12-30 16:23:49 -0300

    Getting favorite courses

--- src/controllers/favoritesController.ts
+++ src/controllers/favoritesController.ts
@@ -3,9 +3,24 @@ import { AuthenticatedRequest } from "../middlewares/auth"
 import { favoriteService } from "../services/favoriteService"
 
 export const favoritesController = {
+    // GET /favorites
+    index: async (req: AuthenticatedRequest, res: Response) => {
+        const userId = req.user!.id // Pega o user autenticado pelo header da requisição
+
+        try {
+            const favorites = await favoriteService.findByUserId(userId)    
+            return res.json(favorites)
+        } 
+        catch (error) {
+            if (error instanceof Error) {
+                return res.status(400).json({ message: error.message }) 
+            } 
+        }
+    },
+
     // POST /favorites
     save: async (req: AuthenticatedRequest, res: Response) => {
-        const userId = req.user!.id
+        const userId = req.user!.id // Pega o user autenticado pelo header da requisição
         const { courseId } = req.body
 
         try {
--- src/services/favoriteService.ts
+++ src/services/favoriteService.ts
@@ -1,6 +1,27 @@
 import { Favorite } from "../models"
 
 export const favoriteService = {
+    findByUserId: async (userId: number) => {
+        const favorites = await Favorite.findAll({
+            attributes: [['user_id', /*as*/ 'userId' ]], // Atributos filtrados retornados na requisição
+            where: { userId: userId }, // Apenas os favoritos do usuário com o id especifico
+            include: {
+                association: 'Course', // Apenas associação do curso pois no front-end serão devolvidos cursos favoritados com suas informações pré carregadas da associação
+                attributes: [   // Atributos filtrados retornados na requisição provenientes da associação
+                    'id',
+                    'name',
+                    'synopsis',
+                    ['thumbnail_url', /*as*/ 'thumbnailUrl']
+                ]
+            }
+        })
+
+        return { // Retorna a resposta de maneira mais organizada em um objeto personalizado
+            userId: userId,
+            courses: favorites.map(favorite => favorite.Course) 
+        }
+    },
+
     create: async (userId: number, courseId: number) => {
         const favorite = Favorite.create({
             courseId: courseId,
